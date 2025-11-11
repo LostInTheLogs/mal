56e2c0d518e6068f78c6a5b335210a82fc306fda
LostInTheLogs <110467150+LostInTheLogs@users.noreply.github.com>
2025-11-11 15:54:41 +0100

    refactor core.cpp

--- impls/mycpp/core.cpp
+++ impls/mycpp/core.cpp
@@ -21,25 +21,17 @@ EvalEnv create_root_env() {
         root_env.set(MalSymbol(name), std::make_shared<MalFunc>(abcd));
     };
 
-    register_func("+", 2, [](MalFuncArgs args) -> shared_ptr<MalType> {
-        auto a = dyn<MalInt>(args[0])->get();
-        auto b = dyn<MalInt>(args[1])->get();
-        return make_shared<MalInt>(a + b);
-    });
-    register_func("-", 2, [](MalFuncArgs args) -> shared_ptr<MalType> {
-        auto a = dyn<MalInt>(args[0])->get();
-        auto b = dyn<MalInt>(args[1])->get();
-        return make_shared<MalInt>(a - b);
-    });
-    register_func("*", 2, [](MalFuncArgs args) -> shared_ptr<MalType> {
-        auto a = dyn<MalInt>(args[0])->get();
-        auto b = dyn<MalInt>(args[1])->get();
-        return make_shared<MalInt>(a * b);
-    });
-    register_func("/", 2, [](MalFuncArgs args) -> shared_ptr<MalType> {
-        auto a = dyn<MalInt>(args[0])->get();
-        auto b = dyn<MalInt>(args[1])->get();
-        return make_shared<MalInt>(a / b);
-    });
+    const auto register_int_func = [register_func](const char* name, auto op) {
+        register_func(name, 2, [op](MalFuncArgs args) {
+            auto a = dyn<MalInt>(args[0])->get();
+            auto b = dyn<MalInt>(args[1])->get();
+            return make_shared<MalInt>(op(a, b));
+        });
+    };
+
+    register_int_func("+", std::plus<>());
+    register_int_func("-", std::minus<>());
+    register_int_func("*", std::multiplies<>());
+    register_int_func("/", std::divides<>());
     return root_env;
 }
